ab469c83eb0d9d7c7623b1f3c391c878d454e505
Carla Morral Martin <31885862+carlaMorral@users.noreply.github.com>
2021-04-18 17:48:51 +0200

    Header finished

---
+++ src/components/CustomHeader.vue
@@ -0,0 +1,58 @@
+<template>
+    <header>
+        <div class="container-fluid p-0">
+            <div class="row">
+                <img class="col-sm-2" alt="Vue logo" src="../assets/logo.jpeg">
+                <span class="title col-sm-2">MedServer</span>
+                <div class="options col-sm">
+                    <span class="option">EL MEU COMPTE</span>
+                    <span class="option">LES MEVES COMANDES</span>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<style>
+header {
+  position: absolute;
+  top: 0;
+  width: 100%;
+  height: auto; /* Set the fixed height of the footer here */
+  background-color: rgb(124, 230, 124);
+
+}
+
+.title {
+    font-size: 3rem;
+    padding: 40px;
+    align-self: center;
+}
+
+.options {
+    align-self: center;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    margin: 30px;
+}
+.option {
+    margin: 10px;
+    border-radius: 5px;
+    transition: all 0.5s ease;
+    padding: 1rem 1rem;
+    font-size: 1.5rem;
+}
+.option:hover {
+    background-color: white;
+    transform: scale(1.1);
+}
+
+</style>
+
+<script>
+export default {
+  name: 'CustomHeader'
+}
+</script>
