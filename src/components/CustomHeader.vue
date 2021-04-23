59aedb89162bf41854d890e387c67c6c4c9f3947
Carla Morral Martin <31885862+carlaMorral@users.noreply.github.com>
2021-04-23 16:51:35 +0200

    responsive text

--- src/components/CustomHeader.vue
+++ src/components/CustomHeader.vue
@@ -3,7 +3,7 @@
         <div class="container-fluid p-0">
             <div class="row">
                 <!--<img class="col-sm-2" alt="Vue logo" src="../assets/logo.jpeg">-->
-                <span class="title col-sm-2">MedServer</span>
+                <span class="title col-sm-4">MedServer</span>
                 <div class="options col-sm">
                     <span class="option">EL MEU COMPTE</span>
                     <span class="option">LES MEVES COMANDES</span>
@@ -19,30 +19,28 @@ header {
   top: 0;
   width: 100%;
   height: auto; /* Set the fixed height of the footer here */
-  background-color: rgb(124, 230, 124);
-
+  background-color: #51c4d3;
 }
 
 .title {
-    font-size: 3rem;
-    padding: 10px;
+    font-size: 4vw;
+    margin-top: 0.5rem;
     align-self: center;
+    font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
 }
 
 .options {
     align-self: center;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
     margin: 10px;
 }
 .option {
     margin: 5px;
     border-radius: 5px;
     transition: all 0.5s ease;
-    padding: 0.5rem 0.5rem;
-    font-size: 1rem;
-    font-weight: bold;
+    margin: 1rem;
+    padding:0.5rem;
+    font-size: 2vw;
+
 }
 .option:hover {
     background-color: white;
