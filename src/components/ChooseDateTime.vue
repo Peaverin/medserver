afc21ef203b0e19d5a5d4343706875371eead503
Eduard <eduvives2212@gmail.com>
2021-04-26 17:23:54 +0200

    Choose date/time fet molt simple i cutre. [Slide 09]

---
+++ src/components/ChooseDateTime.vue
@@ -0,0 +1,50 @@
+<template>
+  <b-container class="choose-date-time">
+    <h1 class="m-5">QUAN?</h1>
+    <b-row class="justify-content-md-center mb-5">
+      <p> Tria quan vols que enviem la comanda</p>
+    </b-row>
+    <b-row class="mb-5" align-h="around">
+      <b-col>
+        <label>Dia de l'enviament</label>
+      </b-col>
+      <b-col>
+        <label>Hora de l'enviament</label>
+      </b-col>
+    </b-row>
+    <b-row class="mb-5" align-h="around">
+      <b-col>
+        <datepicker placeholder="Select Date"></datepicker>
+      </b-col>
+      <b-col>
+        <vue-timepicker :minute-interval="15"></vue-timepicker>
+      </b-col>
+    </b-row>
+    <b-row align-v="end" align-h="around">
+      <b-col>
+        <med-button>Seguir comprant</med-button>
+      </b-col>
+      <b-col>
+        <med-button>Continuar</med-button>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import Datepicker from 'vuejs-datepicker';
+import VueTimepicker from 'vue2-timepicker/src/vue-timepicker.vue'
+import MedButton from './MedButton.vue'
+export default {
+  name: "ChooseDateTime",
+  components: {
+    Datepicker,
+    VueTimepicker,
+    MedButton
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
