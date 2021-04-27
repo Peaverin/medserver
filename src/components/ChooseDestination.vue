47b6f896948ce67c03753891632f42d925f75c6f
Eduard <eduvives2212@gmail.com>
2021-04-27 13:40:32 +0200

    Choose destiantion creat (s'ha de millorar). [Slide 10]

---
+++ src/components/ChooseDestination.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="container-sm px-5">
+    <h1 class="m-5">ON?</h1>
+    <p class="m-5">Tria on vols que enviem la comanda</p>
+    <div class="radio col-sm-10 mb-3">
+      <label><input type="radio" name="optradio" checked> Usar direcció del compte</label>
+    </div>
+    <div class="radio col-sm-10 mb-4">
+      <label><input type="radio" name="optradio"> Usar direcció següent:</label>
+    </div>
+    <div class="form-group row">
+      <label for="usr" class="col-form-label col-sm-4">Carrer, número, pis i porta:</label>
+      <div class="col-sm-6">
+        <input type="text" class="form-control" id="usr">
+      </div>
+    </div>
+    <div class="form-group row">
+      <label for="pwd" class="col-form-label col-sm-4">Codi postal:</label>
+          <div class="col-sm-6">
+        <input type="number" class="form-control" id="pwd">
+      </div>
+    </div>
+    <div class="form-group row">
+      <label for="pwd" class="col-form-label col-sm-4">Municipi:</label>
+      <div class="col-sm-6">
+        <input type="text" class="form-control" id="mun">
+      </div>
+    </div>
+    <div class="row justify-content-center" id="end_buttons">
+      <div class="col-4">
+        <med-button>ENRERE</med-button>
+      </div>
+      <div class="col-4">
+        <med-button>CONTINUAR</med-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import MedButton from './MedButton.vue'
+export default {
+  name: "ChooseDestination",
+  comments: {
+    MedButton
+  }
+}
+</script>
+
+<style scoped>
+.radio{
+  margin-left: auto;
+  text-align: start;
+}
+.col-form-label{
+  text-align: start;
+  margin-left: auto;
+}
+#end_buttons{
+  margin-top: 70px;
+}
+</style>
