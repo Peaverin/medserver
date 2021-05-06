c9d30749abb516547874ad781f5193da2764c2ea
Albert Mir <amirmonc7@alumnes.ub.edu>
2021-05-06 19:47:29 +0200

    responsive footer

--- src/components/CustomFooter.vue
+++ src/components/CustomFooter.vue
@@ -7,6 +7,24 @@
   display: block;
   text-align: center;
 }
+
+/* Small devices (landscape phones, 544px and up) */
+@media (min-width: 300px) {  
+    p {font-size:13px;} /*1rem = 16px*/
+}
+
+/* Medium devices (tablets, 768px and up) The navbar toggle appears at this breakpoint */
+@media (min-width: 768px) {  
+    p {font-size:16px;} /*1rem = 16px*/
+}
+
+/* Large devices (desktops, 992px and up) */
+@media (min-width: 992px) { 
+    p {font-size:18px;} /*1rem = 16px*/
+}
+
+
+
 </style>
 
 <template>
