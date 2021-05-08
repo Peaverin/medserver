5969c4721815e57a427e1ebc8b81a33ef5360504
Albert Mir <amirmonc7@alumnes.ub.edu>
2021-05-08 18:39:26 +0200

    Bordes de finestres de Inicia sessió/registre fora; per coherencia amb la resta ed forms

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -57,7 +57,7 @@ export default {
 
 .navbar-light {
     background-color: #ffffff;
-    box-shadow: 0px 14px 80px rgba(34, 35, 58, 0.2);
+
   }
 
 .vertical-center {
@@ -71,9 +71,9 @@ export default {
     width: 450px;
     margin: auto;
     background: #ffffff;
-    box-shadow: 0px 14px 80px rgba(34, 35, 58, 0.2);
+
     padding: 40px 55px 45px 55px;
-    border-radius: 15px;
+
     transition: all .3s;
   }
 
